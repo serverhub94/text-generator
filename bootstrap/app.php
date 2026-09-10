a1993d76addab7e61ab81cfbb696ee24f7449fff
Vania <galexpert@ukr.net>
2026-09-10 14:28:55 +0300

    Revert bootstrap/app.php to upstream version

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,13 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
+use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
+
+use Illuminate\Support\Facades\Response;
+
+
+
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -20,19 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
         );
     })
-// Внимание !!!! решает проблему ошибки 500 подключает понятную ошибку дебагер Spatie
- /*   ->withExceptions(function (Exceptions $exceptions) {
-        // Патч для дебаггера: ловим ошибки Blade на Windows до падения сервера в 500
-        if (config('app.debug') && !request()->expectsJson()) {
-            $exceptions->reportable(function (\Throwable $e) {
-                while (ob_get_level() > 0) ob_end_clean();
 
-                if (class_exists(\Spatie\LaravelIgnition\IgnitionServiceProvider::class)) {
-                    (new \Spatie\Ignition\Ignition())->handleException($e->getPrevious() ?? $e);
-                    exit;
-                }
-            });
-        }
-    })*/
+
     ->create();
 
